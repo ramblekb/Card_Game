2303c95478b00d8989d1cdcbf4e6c31177ce9fea
BigECire <ericsanders5800@gmail.com>
2019-09-28 11:28:44 -0500

    adding passport

--- crdgame/src/components/Login.js
+++ crdgame/src/components/Login.js
@@ -1,13 +1,58 @@
-import React, { Component } from 'react'
+import React from "react";
+import Axios from "axios";
+import {Redirect, NavLink} from "react-router-dom";
 
-const Login = () => {
-return(<div className="login">
-<h1>Login</h1>
-<form method="post">
-    <input type="text" name="u" placeholder="Username" required="required" />
-    <input type="password" name="p" placeholder="Password" required="required" />
-    <button type="submit" className="btn btn-primary btn-block btn-large">Login</button>
-</form>
-</div>)
+class Login extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+            username: "",
+            password: "",
+            loggedIn: false
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({
+          [event.target.name]: event.target.value,
+        });
+      };
+
+    loginUser = (event)=>{
+        event.preventDefault();
+        Axios.post("/loginUser", {
+            username: this.state.username,
+            password: this.state.password,
+        }).then((res)=>{
+            localStorage.setItem('JWT', res.data.token);
+            this.setState({loggedIn: true})
+            console.log("logged in")
+        }).catch((err)=>{
+            console.log(err)
+        })
+    }
+
+
+    render(){
+        if(!this.state.loggedIn){
+            return <div className="container">
+            <form>
+            <div class="form-group">
+            Username: <input type="text" class="form-control" name="username" value={this.state.username} onChange={this.handleChange} />
+            </div>
+            <div class="form-group">
+            Password: <input type="password" class="form-control" name="password" value={this.state.password} onChange={this.handleChange} />
+            </div>
+            <button type="submit" class="btn btn-success" onClick={this.loginUser}>Login</button>
+            <NavLink to="/signup"> Signup </NavLink>
+        </form>
+        </div>
+        }
+        else {
+            return <Redirect to={{pathname: "/books", state: {loggedIn: true}}}/>
+        }
+    }
+
 }
+
 export default Login;
