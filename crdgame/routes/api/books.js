7f76a3d6fb3222d52773ec4674ae9c5263e36e12
BigECire <ericsanders5800@gmail.com>
2019-10-01 19:06:19 -0500

    disabled passport

--- crdgame/routes/api/books.js
+++ crdgame/routes/api/books.js
@@ -3,11 +3,24 @@ const cardsController = require("../../controllers/cardsController");
 const jwt = require("jsonwebtoken");
 const jwtVerify = require("../../config/jwt");
 
-router.route("/")
-  .get(jwtVerify.confirmToken, jwtVerify.verifyToken, cardsController.findAll);
+// router.route("/")
+//   .get(jwtVerify.confirmToken, jwtVerify.verifyToken, cardsController.findAll)
+//   .post(jwtVerify.confirmToken, jwtVerify.verifyToken, cardsController.create);
 
-router
+  router.route("/")
+  .get(jwtVerify.confirmToken, jwtVerify.verifyToken, cardsController.findAll)
+  .post(jwtVerify.confirmToken, jwtVerify.verifyToken, cardsController.create);
+
+  // router
+  // .route("/:id")
+  // .get(jwtVerify.confirmToken, jwtVerify.verifyToken, cardsController.findById)
+  // .put(jwtVerify.confirmToken, jwtVerify.verifyToken, cardsController.update)
+  // .delete(jwtVerify.confirmToken, jwtVerify.verifyToken, cardsController.remove);
+
+  router
   .route("/:id")
-  .get(jwtVerify.confirmToken, jwtVerify.verifyToken, cardsController.findById);
+  .get(cardsController.findById)
+  .put(cardsController.update)
+  .delete(cardsController.remove);
 
 module.exports = router;
