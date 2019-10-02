1c50325b9253ef1b639d1d19b31ba88cf416b298
BigECire <ericsanders5800@gmail.com>
2019-10-01 19:51:06 -0500

    src is gone

---
+++ crdgame/client/src/components/Title/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css";
+
+
+
+function Title() {
+  return (
+  <div>
+<div className="App-header">
+  <h1 className="title">lisbon card game</h1>
+  </div>
+</div>
+
+
+  )
+}
+
+export default Title;
--- crdgame/client/src/pages/Login.js
+++ crdgame/client/src/pages/Login.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react'
+import React from 'react'
+import Nav from '../components/Nav'
 
 const Login = () => {
-return(<div className="login">
+return(
+
+<div>
+<Nav />
+
+<div className="login">
 <h1>Login</h1>
 <form method="post">
     <input type="text" name="u" placeholder="Username" required="required" />
     <input type="password" name="p" placeholder="Password" required="required" />
     <button type="submit" className="btn btn-primary btn-block btn-large">Login</button>
 </form>
-</div>)
+</div>
+
+</div>
+
+)
 }
 export default Login;
