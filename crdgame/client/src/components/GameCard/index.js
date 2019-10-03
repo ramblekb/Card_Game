d782d5fb140589dae5c74f891901385d754c8b6e
BigECire <ericsanders5800@gmail.com>
2019-10-03 18:29:12 -0500

    maped card

--- crdgame/client/src/components/GameCard/index.js
+++ crdgame/client/src/components/GameCard/index.js
@@ -2,25 +2,25 @@ import React from "react";
 import "./style.css";
 
 // insert props in DeckCard
-function GameCard() {
+function GameCard(props) {
   return (
 
 
     
 
-    <div className="row">
+    // <div className="row">
 
-      {/* <div classname="container"> */}
-      {/* <div className="col s12 m6 s2"> */}
-      <div class="card">
+      // {/* <div classname="container"> */}
+      // {/* <div className="col s12 m6 s2"> */}
+      <div class="card" data-id= {props.Card.id}>
         <div class="card-image">
       {/* <div className="card" id="deckCard" onClick=""> */}
       {/* <span class="card-title">name of card</span> */}
       {/* <div className="img-container"> */}
-      <span className="card-title">card name</span>
+      <span className="card-title">{props.Card.name}</span>
       {/* <h1 className="tote">Card Name</h1> */}
 
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/OSIRIS_Mars_true_color.jpg/1024px-OSIRIS_Mars_true_color.jpg" alt="" />
+        <img src={props.Card.img} alt="" />
 
         {/* <ul className="collection">
       <li className="collection-item">Cost<span className="badge">0</span></li>
@@ -30,21 +30,21 @@ function GameCard() {
     {/* <div class="card-content"> */}
         
 <ul className="collection">
-      <li className="collection-item">Cost<span className="badge">0</span></li>
-      <li className="collection-item">Demage<span className="badge">1</span></li>      
+      <li className="collection-item">Cost<span className="badge">{props.Card.cost}</span></li>
+      <li className="collection-item">Damage<span className="badge">{props.Card.damage}</span></li>      
     </ul> 
 
         </div>
         </div>
 
- {/* </div> */}
-{/* <div className="card-content">
-<ul className="collection">
-      <li className="collection-item">Cost<span className="badge">0</span></li>
-      <li className="collection-item">Demage<span className="badge">1</span></li>      
-    </ul>
-        </div> */}
-</div>
+//  {/* </div> */}
+// {/* <div className="card-content">
+// <ul className="collection">
+//       <li className="collection-item">Cost<span className="badge">0</span></li>
+//       <li className="collection-item">Demage<span className="badge">1</span></li>      
+//     </ul>
+//         </div> */}
+// // </div>
 // </div>
   // </div>
   //  </div>
--- crdgame/client/src/pages/Deck.js
+++ crdgame/client/src/pages/Deck.js
@@ -1,27 +1,65 @@
 import React, { Component } from 'react'
+import axios from "axios";
 import DeckCard from '../components/DeckCard'
 import PickedDeck from '../components/PickedDeck'
 import Title from '../components/Title'
 import Nav from '../components/Nav'
+import Cookies from 'js-cookie';
 
-class Books extends Component {
+class Deck extends Component {
   state = {
-    books: [],
-    title: "",
-    author: "",
-    synopsis: ""
+    allCard: [],
+    deck: [],
+    userId: []
   };
+  componentDidMount() {
+    this.loadCards();
+  }
+
+  // Loads all books  and sets them to this.state.books
+  loadCards = () => {
+    // let accessString = localStorage.getItem('JWT');
+    // if(accessString == null){
+    //   accessString = Cookies.get('JWT');
+    // }
+    // axios.get("/api/cards",{headers: { Authorization: `JWT ${accessString}` }})
+    axios.get("/api/cards")
+      .then(res =>
+        this.setState({ loggedIn: true, allCard: res.data })
+      )
+      .catch(err => console.log(err));
+
+    // axios.get("/api/users/" + 1)
+    //   .then(res =>{
+    //     this.setState({ userId: res.data })
+    //     console.log(this.state.allCard)
+    //   }
+    //   )
+    //   .catch(err => console.log(err));
+  };
+
+  cardClicked = event => {
+    console.log(this.state.deck)
+    let selected = event.target.attributes.getNamedItem("data-id").value
+    axios.get("/api/users/" + 1)
+      .then(res =>{
+        let yourDeck = this.state.deck; 
+        yourDeck.push(res.data)
+        this.setState({ deck: yourDeck })}
+      )
+      .catch(err => console.log(err));
+  }
 
   render() {
     return (
       <div>
         <Nav />
+
         <Title />
 
-        <DeckCard />
+        <DeckCard allCard={this.state.allCard}/>
 
         <PickedDeck />
-
 
       </div>
     )
