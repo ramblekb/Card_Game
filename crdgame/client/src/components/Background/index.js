71eb0c557d7e719b0c84d61913b85d877bdfbc17
Kip Bradley Smith <ramblekb@gmail.com>
2019-10-03 20:09:35 -0500

    making changes

--- crdgame/client/src/components/Background/index.js
+++ crdgame/client/src/components/Background/index.js
@@ -10,3 +10,151 @@ for (var r =0,i=0; i<50; i++, r = 100-i*2)
         stroke=hsla(${rnd(360)},${60+rnd(25)}%,${60+rnd(25)}%,0.8) 
         stroke-dasharray="${Array(2+rnd(r)).fill(0).map(e=>rnd(r)).join(' ')}" r=${r} fill=none ></circle>`
 
+
+
+
+
+        // import React from 'react'
+// // import "./style.css";
+
+
+// function YouPlay(){
+
+// // maybe onClick event listener here 
+//   return (
+//     <div>
+    
+//     <div className="container-fluid">
+    
+//     <h1 className="YouPlay">Play One of Your Space Cards</h1>
+//     <div className="divider"></div>
+    
+ 
+
+//     <div className="row">
+//       <div classname="container">
+//       <div className="col s12 m6 s2">
+//       <div className="card" onClick="">
+//       <div className="img-container">
+//         <img src="images/sample-1.jpg" alt="" />
+// </div>
+// </div>
+// </div>
+//   </div>
+//   </div>
+    
+//   <div className="row">
+//       <div classname="container">
+//       <div className="col s12 m6 s2">
+//       <div className="card" onClick="">
+//       <div className="img-container">
+//         <img src="images/sample-1.jpg" alt="" />
+// </div>
+// </div>
+// </div>
+//   </div>
+//   </div>
+
+//   <div className="row">
+//       <div classname="container">
+//       <div className="col s12 m6 s2">
+//       <div className="card" onClick="">
+//       <div className="img-container">
+//         <img src="images/sample-1.jpg" alt="" />
+// </div>
+// </div>
+// </div>
+//   </div>
+//   </div>
+//   </div>
+
+
+ 
+// </div>
+
+  
+
+//   )
+// }
+// export default YouPlay;
+
+
+
+
+import React from 'react'
+import "./style.css";
+import Mars from '../mars.jpg'
+
+
+
+function BattleDeck(){
+
+// maybe onClick event listener here 
+  return (
+    <div>
+    
+    <div className="container-fluid">
+    <div className="float-right">
+    {/* <a className="waves-effect waves-light btn">Save Deck</a> */}
+    </div>
+             
+    <h1 className="pickedDeck">VS CARD</h1>
+    
+    <div className="divider"></div>
+    
+ 
+    <div className="row">
+
+      {/* <div classname="container" id="decker">
+      <div className="col s12 m6 s2">
+      <div className="card" onClick="">
+      <div className="con">CardName</div>
+      <div className="img-container">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/OSIRIS_Mars_true_color.jpg/1024px-OSIRIS_Mars_true_color.jpg" alt="smiley" />
+</div>
+      <div className="con">0</div>
+
+</div>
+</div>
+  </div> */}
+
+  <div className="row">
+      <div classname="container">
+      <div className="col s12 m6 s2">
+      <div className="card" id="deckCard" onClick="">
+      <div className="img-container">
+        <img src={Mars} alt="" />
+</div>
+</div>
+</div>
+  </div>
+  </div>
+
+  
+
+
+  
+
+  {/* <div className="row">
+      <div classname="container">
+      <div className="col s12 m6 s2">
+      <div className="card" id="deckCard" onClick="">
+      <div className="img-container">
+        <img src="images/sample-1.jpg" alt="" />
+</div>
+</div>
+</div>
+  </div>
+  </div> */}
+    
+  
+
+  
+  </div>
+  </div>
+  </div>
+
+  )
+}
+export default Background;
+
