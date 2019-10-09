c78054f8623f2c12593299b5b02bf3e1ea04b82e
BigECire <ericsanders5800@gmail.com>
2019-10-08 21:39:43 -0500

    deck fully functional and passport done

--- crdgame/routes/api/users.js
+++ crdgame/routes/api/users.js
@@ -3,24 +3,24 @@ const usersController = require("../../controllers/usersController");
 const jwt = require("jsonwebtoken");
 const jwtVerify = require("../../config/jwt");
 
-// router.route("/")
-//   .get(jwtVerify.confirmToken, jwtVerify.verifyToken, usersController.findAll)
-//   .post(jwtVerify.confirmToken, jwtVerify.verifyToken, usersController.create);
+router.route("/")
+  .get(jwtVerify.confirmToken, jwtVerify.verifyToken, usersController.findAll)
+  .post(jwtVerify.confirmToken, jwtVerify.verifyToken, usersController.create);
 
-  router.route("/")
-  .get(usersController.findAll)
-  .post(usersController.create);
-
-  // router
-  // .route("/:id")
-  // .get(jwtVerify.confirmToken, jwtVerify.verifyToken, usersController.findById)
-  // .put(jwtVerify.confirmToken, jwtVerify.verifyToken, usersController.update)
-  // .delete(jwtVerify.confirmToken, jwtVerify.verifyToken, usersController.remove);
+  // router.route("/")
+  // .get(usersController.findAll)
+  // .post(usersController.create);
 
   router
   .route("/:id")
-  .get(usersController.findById)
-  .put(usersController.update)
-  .delete(usersController.remove);
+  .get(jwtVerify.confirmToken, jwtVerify.verifyToken, usersController.findById)
+  .put(jwtVerify.confirmToken, jwtVerify.verifyToken, usersController.update)
+  .delete(jwtVerify.confirmToken, jwtVerify.verifyToken, usersController.remove);
+
+  // router
+  // .route("/:id")
+  // .get(usersController.findById)
+  // .put(usersController.update)
+  // .delete(usersController.remove);
 
 module.exports = router;
